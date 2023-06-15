393e52fa8c4a51d223d40ec4ce0091c91c70e2b0
medjamaai <simojamaai@gmail.com>
2023-06-15 15:44:36 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main to write
+ * how to write a sentence in c
+ * always return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
