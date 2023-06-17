a90fcd9f0556964ea2cc37f6ce6d856f097ccc93
medjamaai <simojamaai@gmail.com>
2023-06-16 17:56:25 -0700

    variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Variables
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 10; n++)
+	printf("%d", n);
+	printf("\n");
+	return (0);
+}
