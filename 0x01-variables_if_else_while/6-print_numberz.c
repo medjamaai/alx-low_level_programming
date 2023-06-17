97e0ad7f6de849f6cad0c358e90d54835c905b5f
medjamaai <simojamaai@gmail.com>
2023-06-16 18:07:32 -0700

    variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Variables
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int  alpha;
+
+	for (alpha = 48; alpha <= 57; alpha++)
+	putchar(alpha);
+	putchar('\n');
+	return (0);
+}
