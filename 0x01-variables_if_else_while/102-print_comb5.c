a2fbf1af10cfa2acbf2898936abd14add4ca583e
medjamaai <simojamaai@gmail.com>
2023-06-18 07:56:34 -0700

    variables ALx

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Variables
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int  alpha, j, k, i;
+
+	for (alpha = 48; alpha <= 57; alpha++)
+	{
+	for (j = 48; j <= 57; j++)
+	{
+	for (k = alpha; k <= 57; k++)
+	{
+	for (i = j + 1; i <= 57; i++)
+	{
+	putchar(alpha);
+	putchar(j);
+	putchar(32);
+	putchar(k);
+	putchar(i);
+	if (alpha != 57 || j != 56 || k != 57 || i != 57)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
