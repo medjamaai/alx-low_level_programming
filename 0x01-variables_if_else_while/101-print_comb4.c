d45d48cd5561b4aaa40bf5a773b324fc1072c87f
medjamaai <simojamaai@gmail.com>
2023-06-18 07:10:02 -0700

    variables advanc

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Variables
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int  alpha, j, k;
+
+	for (alpha = 48; alpha <= 57; alpha++)
+	{
+	for (j = alpha + 1; j <= 57; j++)
+	{
+	for (k = j + 1; k <= 57; k++)
+	{
+	putchar(alpha);
+	putchar(j);
+	putchar(k);
+	if (alpha != 56 || j != 57)
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
