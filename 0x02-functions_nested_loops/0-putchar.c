c911af71aa6a374c33a2bf8be979ec3b1591cb5b
medjamaai <simojamaai@gmail.com>
2023-06-20 06:07:28 -0700

    functions alx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - functions
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+	{
+	printf("_putchar!\n");
+	return (0);
+	}
