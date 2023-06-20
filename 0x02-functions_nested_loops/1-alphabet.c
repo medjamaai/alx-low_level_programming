c244727fbf7d75e29fbf32498d8db4f5dceda9e3
medjamaai <simojamaai@gmail.com>
2023-06-20 16:04:18 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_alphabet - functions
+ *
+ * Return: void (Success)
+*/
+void print_alphabet(void);
+
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	_putchar(alpha);
+	}
+	_putchar('\n');
+}	
