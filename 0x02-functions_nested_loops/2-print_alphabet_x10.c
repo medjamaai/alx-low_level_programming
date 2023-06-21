39e90c8f617d4c8df77f6f0a2c4e3de9f517bb01
medjamaai <simojamaai@gmail.com>
2023-06-21 04:05:23 -0700

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,14 @@
-#include "main.h"
-
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include"main.h"
+/* betty style doc for function print_alphabet_x10 goes there */
 /**
- * print_alphabet_X10 - functions for print 10 times
+ * print_alphabet_x10 - function to print the alphabet 10 times
  *
- * Return: void (Success)
-*/
-void print_alphabet_x10(void)
-
+ * Return: void
+ */
+void print_alphabet(void)
 {
 	int alpha, j;
-	
+
 	for (j = 0; j < 10; j++)
 	{
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
@@ -20,5 +16,5 @@ void print_alphabet_x10(void)
 	_putchar(alpha);
 	}
 	_putchar('\n');
-	}
+}
 }
