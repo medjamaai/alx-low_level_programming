eb4a1c7649b7d9d2de9f1010bee1638dd62a1a15
medjamaai <simojamaai@gmail.com>
2023-06-22 11:01:15 -0700

    functions part 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * mul - function for multiplication of @a and @b 
+ *
+ * @a : numbers that want to multiplie
+ * @b : numbers that want to multiplie
+ * Return: multiplication of @a and @b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
