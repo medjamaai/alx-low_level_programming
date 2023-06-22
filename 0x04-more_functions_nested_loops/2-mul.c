ec1548250acf2ff9c0d282bc7d1b1496a429663a
medjamaai <simojamaai@gmail.com>
2023-06-22 11:08:39 -0700

    functions part 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,7 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- * mul - function for multiplication of @a and @b 
- *
+ * mul - function for multiplication of @a and @b
  * @a : numbers that want to multiplie
  * @b : numbers that want to multiplie
  * Return: multiplication of @a and @b
