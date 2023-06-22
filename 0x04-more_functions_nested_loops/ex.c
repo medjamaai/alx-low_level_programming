1d716089d762ec77d87f4937666671bdcf0b5a3f
medjamaai <simojamaai@gmail.com>
2023-06-22 11:53:49 -0700

    variables functions

---
+++ 0x04-more_functions_nested_loops/ex.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * *** - function for multiplication of @a and @b
+ * @a : numbers that want to multiplie
+ * @b : numbers that want to multiplie
+ * Return: multiplication of @a and @b
+*/
+
+{
+	return ();
+}
