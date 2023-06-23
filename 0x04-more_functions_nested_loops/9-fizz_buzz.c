e2f0b5440cb0f442f4a5abc8fb8550d18f6bdb63
medjamaai <simojamaai@gmail.com>
2023-06-23 12:32:53 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Variables
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int  i;
+	char buzz[] = "Buzz";
+	char fizz[] = "Fizz";
+
+	for (i = 1; i <= 99; i++)
+	{
+	if (i % 3 != 0 && i % 5 != 0)
+	printf("%d", i);
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("%s%s", fizz, buzz);
+	}
+	else if (i % 3 == 0)
+	printf("%s", fizz);
+	else if (i % 5 == 0)
+	printf("%s", buzz);
+	printf(" ");
+	}
+	return (0);
+}
