8d82747469bbb75a11bfbdaec9429558e07aa90f
medjamaai <simojamaai@gmail.com>
2023-06-23 02:15:45 -0700

    functions 2

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_numbers - function for prints the numbers.
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_line - function for multiplication of @a and @b
+ * @n : number of lines
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	_putchar('_');
+	_putchar('\n');
+}
