a1c8062fbe4588159a03023096ec95e2e276ad55
medjamaai <simojamaai@gmail.com>
2023-06-26 02:51:31 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset to 98
+ *
+ * @n : number that will be reset
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointer is comming
