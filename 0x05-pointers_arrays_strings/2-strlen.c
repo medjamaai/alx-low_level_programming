abb5790d4e21410e8010d5935e2379bafdc74938
medjamaai <simojamaai@gmail.com>
2023-06-26 03:29:07 -0700

    functions

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - mesure a lenght of char
+ *
+ * @s : character that will be mesured
+ * Return: always len + 1 : lenght of character
+ */
+int _strlen(char *s)
+{
+	int len, i;
+
+	len = 0;
+	for (i = 0; i <= 100; i++)
+	{
+	if (*(s + i) != '\0')
+	len = len + 1;
+	else if (*(s + i) == '\0')
+	break;
+	}
+	return (len);
+}
