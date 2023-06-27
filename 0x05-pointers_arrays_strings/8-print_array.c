ad069b42e7f46c57dcc2b63954146714bba3d2ca
medjamaai <simojamaai@gmail.com>
2023-06-27 07:20:25 -0700

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if(n == 0)
+	printf("%d", *a);
+	else
+	{
 	printf("%d", *a);
 	for (i = 1; i < n; i++)
 	{
 	a++;
 	printf(", %d", *a);
+	}
 	}
 	printf("\n");
 }
