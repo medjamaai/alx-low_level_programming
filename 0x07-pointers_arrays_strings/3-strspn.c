81ddc5988d2d5bf0b5e6114074d5cb4148be2c27
medjamaai <simojamaai@gmail.com>
2023-07-04 12:09:46 -0700

    pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,19 +9,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, str;
+	unsigned int i, j, c, str;
 
 	str = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	check = 0;
+	c = 1;
 	for (j = 0; accept[j] != '\0'; j++)
 	{
 	if (accept[j] == s[i])
 	{
+	c = 0;
 	str++;
-	}	
 	}
+	}
+	if (c == 1)
+	return (str);
 	}
 	return (str);
 }
