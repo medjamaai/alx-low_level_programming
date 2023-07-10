5f13fae2e235a5e11f39cf13cda91ec2053c6249
medjamaai <simojamaai@gmail.com>
2023-07-10 02:09:18 -0700

    allocation dynamique

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - code function
+ * @size: size of array
+ * @c: char to input
+ * Return: pointer to array, NULL if fail
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(sizeof(char) * size);
+
+	if (size == 0 || arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Malloc espace reserve
