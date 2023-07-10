36a1645289f56c1299e66eb72dd32e45be198715
medjamaai <simojamaai@gmail.com>
2023-07-10 02:37:38 -0700

    allocation dynamic

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *cp;
+	int size;
+
+	size = strlen(str);
+	if (str == NULL || size == 0)
+		return (NULL);
+	cp = malloc(size * sizeof(char));
+	strcpy(cp, str);
+	return (cp);
+}
