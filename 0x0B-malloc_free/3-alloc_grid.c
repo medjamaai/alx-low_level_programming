fdec06cab85fdcb9f19d5966d0efc346e8cd9061
medjamaai <simojamaai@gmail.com>
2023-07-11 02:59:31 -0700

    allocation dynamic

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - function code
+ * @width: input
+ * @height: input
+ * Return: pointer to 2 dim.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array2;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array2 = malloc(sizeof(int *) * height);
+
+	if (array2 == NULL)
+		return (NULL);
+
+	for (x = 0; x < height; x++)
+	{
+		array2[x] = malloc(sizeof(int) * width);
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			array2[x][y] = 0;
+	}
+
+	return (array2);
+}
