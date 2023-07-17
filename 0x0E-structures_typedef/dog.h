613ed8fa949f7492b69c100bf05fe2ee4d0d8956
medjamaai <simojamaai@gmail.com>
2023-07-17 05:36:21 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
-    char* name;
-    float age;
-    char* owner;
+/**
+ * struct dog - dog structure
+ * @name : input
+ * @age : input
+ * @owner : input
+ */
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
 };
 
 #endif /* DOG_H */
